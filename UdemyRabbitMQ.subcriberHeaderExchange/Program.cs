0d8065d54f67e516dd02fc38de02ccb6d327b252
esrasnck <esrasancak@hotmail.com>
2021-05-13 20:36:36 +0300

    hey

--- UdemyRabbitMQ.subcriberHeaderExchange/Program.cs
+++ UdemyRabbitMQ.subcriberHeaderExchange/Program.cs
@@ -2,14 +2,14 @@
 using RabbitMQ.Client.Events;
 using System;
 using System.Text;
-using System.Threading;
 
-namespace UdemyRabbitMQ.subcriberTopicExchange
+namespace UdemyRabbitMQ.subcriberHeaderExchange
 {
     class Program
     {
         static void Main(string[] args)
         {
+
             var factory = new ConnectionFactory();
             factory.Uri = new Uri("amqps://evrdybgg:TQLBLVPPb7_OR1qBR3hajFa7ncOrB_HM@fish.rmq.cloudamqp.com/evrdybgg");
 
@@ -27,10 +27,10 @@ namespace UdemyRabbitMQ.subcriberTopicExchange
             var queueName = channel.QueueDeclare().QueueName; // channel üzerinden random bir kuyruk ismi bana geliyor.
 
             // var routeKey = "*.Error.*";
-           // var routeKey = "*.*.Warning";
-            var routeKey = "Error.#";  // bu kısımda hata var.
+            // var routeKey = "*.*.Warning";
+            var routeKey = "Info.#";  // bu kısımda hata var.
 
-            channel.QueueBind(queueName,"logs-topic",routeKey);     // bind edecez. subrciber düştüğünde kuyruk da düşsün.
+            channel.QueueBind(queueName, "logs-topic", routeKey);     // bind edecez. subrciber düştüğünde kuyruk da düşsün.
 
             channel.BasicConsume(queueName, false, consumer);   // hata burada. ben bastıramıyorum :(
 
@@ -48,15 +48,6 @@ namespace UdemyRabbitMQ.subcriberTopicExchange
                 channel.BasicAck(e.DeliveryTag, false);
 
             };
-
-
-      /*
-       * önce consume kısmını çalıştırmakta fayda var. yada consume ile publisher ı aynı anda çalıştırmak gerek. yoksa publisherde üretilen mesajlar, herhangi bir kuyruk olmadı için boşa gidiyor. consumer da bunu dinlemiypr.
-       * 
-       * */
-
-
-            Console.ReadLine();
         }
     }
 }
---
+++ demyRabbitMQ.publisherHeaderExchange/Program.cs
@@ -0,0 +1,50 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Linq;
+using System.Text;
+
+namespace demyRabbitMQ.publisherHeaderExchange
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory();
+            factory.Uri = new Uri("amqps://evrdybgg:TQLBLVPPb7_OR1qBR3hajFa7ncOrB_HM@fish.rmq.cloudamqp.com/evrdybgg");
+
+            using var connection = factory.CreateConnection();
+
+            var channel = connection.CreateModel();
+
+
+            // artık kuyruğu sildik. onun yerine exchange oluşturcaz.
+            channel.ExchangeDeclare("logs-fanout", durable: true, type: ExchangeType.Fanout); // tek bunu ekledik
+
+            //1) exchange ismi. 2) fiziksel olarak kaydedilsin. uygulama restart attığında, bu exchange kaybolmasın. false'a set edersem, uygulama restart ettiğinde tüm exchangeler kaybolur. 3) exchange'in tipi. 
+
+
+            Enumerable.Range(1, 50).ToList().ForEach(x =>
+            {
+
+                string message = $"log{x}";
+
+
+
+
+                var messageBody = Encoding.UTF8.GetBytes(message);
+
+                channel.BasicPublish("logs-fanout", "", null, messageBody);
+                //1) exchange ismi, 2) herhangi bir filtreleme olmadığından boş veriyoruz. 3) basic propert olarak null'ı set ediyoruz. sonra da messageBody i yazıyoruz.
+                Console.WriteLine($"Mesaj gönderilmiş.: {message}");
+
+            });
+
+
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
